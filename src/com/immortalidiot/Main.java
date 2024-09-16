07c5c04ad771f10ec4c5e117d154e35ea7ccf1e0
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-17 00:33:29 +0300

    feat: add minion tests

--- src/com/immortalidiot/Main.java
+++ src/com/immortalidiot/Main.java
@@ -2,6 +2,30 @@ package com.immortalidiot;
 
 public class Main {
     public static void main(String[] args) {
+        checkWithMinionType();
+    }
+
+    private static void checkWithMinionType() {
+        DoubledLinkedList<Minion> minions = new DoubledLinkedList<>();
+
+        Minion minion = new Minion("Cферик", 2);
+        Minion otherMinion = new Minion("Глеб", 4);
+
+        minions.add(new Minion("Фиксик", 15));
+        minions.add(minion);
+        minions.add(new Minion("Псиныч", 11));
+
+        minions.printAll();
+        System.out.println("Изменяем " + minions.getElement(1) + " на " + otherMinion);
+        minions.setValue(otherMinion, 1);
+        minions.printAll();
+
+        System.out.println("Удаляем " + otherMinion);
+        minions.remove(otherMinion);
+        minions.printAll();
+    }
+
+    private static void checkWithStringType() {
         DoubledLinkedList<String> doubledLinkedList = new DoubledLinkedList<>();
         doubledLinkedList.add("Один");
         doubledLinkedList.add("Два");
@@ -9,34 +33,22 @@ public class Main {
 
         System.out.println("Size - " + doubledLinkedList.size());
 
-        System.out.println("Все значения #1:");
-        for (String value : doubledLinkedList) {
-            System.out.println(value);
-        }
+        doubledLinkedList.printAll();
 
         doubledLinkedList.setValue("Четыре", 2);
-        System.out.println("Все значения #2:");
-        for (String value : doubledLinkedList) {
-            System.out.println(value);
-        }
+        doubledLinkedList.printAll();
 
         System.out.println("2 элемент: " + doubledLinkedList.getElement(1));
 
         doubledLinkedList.remove((doubledLinkedList.size() - 1));
-        System.out.println("Все значения #3:");
-        for (String value : doubledLinkedList) {
-            System.out.println(value);
-        }
+        doubledLinkedList.printAll();
 
         while (doubledLinkedList.listIterator().hasNext()) {
             doubledLinkedList.remove(doubledLinkedList.listIterator().next());
         }
 
-        System.out.println("Все значения #4:");
         if (!doubledLinkedList.isEmpty()) {
-            for (String value : doubledLinkedList) {
-                System.out.println(value);
-            }
+            doubledLinkedList.printAll();
         } else {
             System.out.println("Значений нет");
         }

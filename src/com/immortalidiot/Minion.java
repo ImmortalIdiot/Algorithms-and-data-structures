442c4fe42e573a1189c77c72f7529889c3fd982a
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-01 01:07:44 +0300

    refactor: beautifully designed toString

--- src/com/immortalidiot/Minion.java
+++ src/com/immortalidiot/Minion.java
@@ -15,10 +15,18 @@ public class Minion {
 
     @Override
     public String toString() {
-        return "Minion: " +
-                "minionName='" + minionName + '\'' +
-                ", eyesCount=" + eyesCount + '\n' +
-                ", age=" + age;
+        String eyeWord = (eyesCount == 1) ? "глазом" : "глазами";
+
+        String yearWord;
+        if (age % 10 == 1 && age % 100 != 11) {
+            yearWord = "год";
+        } else if (age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) {
+            yearWord = "года";
+        } else {
+            yearWord = "лет";
+        }
+
+        return "Миньон " + minionName + " с " + eyesCount + " " + eyeWord + " и возрастом " + age + " " + yearWord + ".";
     }
 
     public int compareTo(Minion otherMinion) {

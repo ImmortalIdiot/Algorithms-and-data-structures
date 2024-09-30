8a4b20c94f5eac94555c08d7b5d68e7b7e8c984d
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-30 12:06:39 +0300

    refactor: add the age field to the minion

--- src/com/immortalidiot/Minion.java
+++ src/com/immortalidiot/Minion.java
@@ -5,16 +5,24 @@ public class Minion {
 
     private final String minionName;
     private final int eyesCount;
+    private int age;
 
     public Minion(String minionName, int eyesCount) {
         this.minionName = minionName;
         this.eyesCount = eyesCount;
     }
 
+    public Minion(String minionName, int eyesCount, int age) {
+        this.minionName = minionName;
+        this.eyesCount = eyesCount;
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "Minion: " +
                 "minionName='" + minionName + '\'' +
-                ", eyesCount=" + eyesCount;
+                ", eyesCount=" + eyesCount + '\n' +
+                ", age=" + age;
     }
 }

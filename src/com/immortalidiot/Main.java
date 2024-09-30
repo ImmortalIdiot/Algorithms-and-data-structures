19ad9350047dae7e7e839b4cf31fcb6446bab6d6
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-01 01:13:29 +0300

    feat: add tests to check stack work

--- src/com/immortalidiot/Main.java
+++ src/com/immortalidiot/Main.java
@@ -2,54 +2,25 @@ package com.immortalidiot;
 
 public class Main {
     public static void main(String[] args) {
-        Minion minion1 = new Minion("Bob", 25);
-        Minion minion2 = new Minion("Kevin", 14);
-        Minion minion3 = new Minion("Stuart", 7);
-        Minion minion4 = new Minion("Rayan", 3);
-        Minion minion5 = new Minion("Fixer", 1);
+        testStack();
+    }
 
-        DoubledLinkedList<Minion> list = new DoubledLinkedList<>();
+    public static void testStack() {
+        CustomStack<Minion> minionStack = new CustomStack<>();
 
-        list.insertHead(minion1);
-        list.insertTail(minion2);
-        list.insertHead(minion3);
-        list.insertTail(minion4);
-        list.insertTail(minion5);
+        minionStack.push(new Minion("Bob", 2, 5));
+        minionStack.push(new Minion("Kevin", 1, 8));
+        minionStack.push(new Minion("Stuart", 2, 3));
+        minionStack.push(new Minion("Rayan", 3, 7));
+        minionStack.push(new Minion("Fixer", 2, 5));
 
-        System.out.println("Весь список");
-        list.printList();
-        System.out.println();
+        minionStack.printStack();
 
-        System.out.println("Размер");
-        System.out.println(list.size());
-        System.out.println();
+        System.out.println("\nВерхний элемент: " + minionStack.peek());
 
-        System.out.println("Заменяем первый");
-        list.replaceFirst(new Minion("Максим", 2));
-        list.printList();
-        System.out.println();
+        System.out.println("\nУдалён " + minionStack.pop());
+        System.out.println("Удалён " + minionStack.pop() + "\n");
 
-        System.out.println("Заменяем последний");
-        list.replaceLast(new Minion("Никита", 1));
-        list.printList();
-        System.out.println();
-
-        System.out.println("Заменяем Bob");
-        list.replace(minion1, new Minion("Глеб", 25));
-        list.printList();
-        System.out.println();
-
-        System.out.println("Удаляем первый");
-        list.removeFirst();
-        list.printList();
-        System.out.println();
-
-        System.out.println("Удаляем последний");
-        list.removeLast();
-        list.printList();
-        System.out.println();
-
-        System.out.println();
-        System.out.println("Размер после удаления\n" + list.size());
+        minionStack.printStack();
     }
 }

670c9647a65add4b0df1abdfd8e16c729723acc8
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-01 00:50:38 +0300

    feat: add comparator to the minion class

--- src/com/immortalidiot/Minion.java
+++ src/com/immortalidiot/Minion.java
@@ -5,12 +5,7 @@ public class Minion {
 
     private final String minionName;
     private final int eyesCount;
-    private int age;
-
-    public Minion(String minionName, int eyesCount) {
-        this.minionName = minionName;
-        this.eyesCount = eyesCount;
-    }
+    private final int age;
 
     public Minion(String minionName, int eyesCount, int age) {
         this.minionName = minionName;
@@ -24,5 +19,15 @@ public class Minion {
                 "minionName='" + minionName + '\'' +
                 ", eyesCount=" + eyesCount + '\n' +
                 ", age=" + age;
+    }
+
+    public int compareTo(Minion otherMinion) {
+        int eyesComparing = Integer.compare(this.eyesCount, otherMinion.eyesCount);
+        if (eyesComparing != 0) { return eyesComparing; }
+
+        int ageComparing = Integer.compare(this.age, otherMinion.age);
+        if (ageComparing != 0) { return ageComparing; }
+
+        return this.minionName.compareTo(otherMinion.minionName);
     }
 }

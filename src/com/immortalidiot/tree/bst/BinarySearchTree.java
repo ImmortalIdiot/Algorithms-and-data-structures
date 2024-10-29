d3b34ef98c6c042c7220ad2e78e98adbe3656702
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-29 21:49:11 +0300

    chore: reformat code

--- src/com/immortalidiot/tree/bst/BinarySearchTree.java
+++ src/com/immortalidiot/tree/bst/BinarySearchTree.java
@@ -18,10 +18,11 @@ public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinary
             return new Node<>(element);
         }
 
-        int comparison = element.compareTo(current.value);
-        if (comparison < 0) {
+        int compare = element.compareTo(current.value);
+
+        if (compare < 0) {
             current.leftChild = insertRecursive(current.leftChild, element);
-        } else if (comparison > 0) {
+        } else if (compare > 0) {
             current.rightChild = insertRecursive(current.rightChild, element);
         }
         return current;
@@ -29,48 +30,52 @@ public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinary
 
     @Override
     public boolean contains(E element) {
-        return containsRecursive(root, element);
+        return recursiveContains(root, element);
     }
 
-    private boolean containsRecursive(Node<E> current, E element) {
+    private boolean recursiveContains(Node<E> current, E element) {
         if (current == null) {
             return false;
         }
 
-        int comparison = element.compareTo(current.value);
-        if (comparison == 0) {
+        int compare = element.compareTo(current.value);
+
+        if (compare == 0) {
             return true;
-        } else if (comparison < 0) {
-            return containsRecursive(current.leftChild, element);
+        } else if (compare < 0) {
+            return recursiveContains(current.leftChild, element);
         } else {
-            return containsRecursive(current.rightChild, element);
+            return recursiveContains(current.rightChild, element);
         }
     }
 
     @Override
     public AbstractBinarySearchTree<E> search(E element) {
-        Node<E> node = searchRecursive(root, element);
+        Node<E> node = recursiveSearching(root, element);
+
         if (node == null) {
             return null;
         }
 
-        BinarySearchTree<E> subtree = new BinarySearchTree<>();
-        subtree.root = node;
-        return subtree;
+        BinarySearchTree<E> branch = new BinarySearchTree<>();
+        branch.root = node;
+
+        return branch;
     }
 
-    private Node<E> searchRecursive(Node<E> current, E element) {
+    private Node<E> recursiveSearching(Node<E> current, E element) {
         if (current == null) {
             return null;
         }
 
-        int comparison = element.compareTo(current.value);
-        if (comparison == 0) {
+        int compare = element.compareTo(current.value);
+
+        if (compare == 0) {
             return current;
-        } else if (comparison < 0) {
-            return searchRecursive(current.leftChild, element);
+        } else if (compare < 0) {
+            return recursiveSearching(current.leftChild, element);
         } else {
-            return searchRecursive(current.rightChild, element);
+            return recursiveSearching(current.rightChild, element);
         }
     }
 
@@ -96,18 +101,18 @@ public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinary
 
     //14 вариант: Метод для поиска максимальной суммы пути в бинарном дереве
     public int maxPathSum() {
-        int[] maxSum = new int[] { Integer.MIN_VALUE };
-        maxPathSumHelper(root, maxSum);
+        int[] maxSum = new int[]{Integer.MIN_VALUE};
+        calculateMaxPathSum(root, maxSum);
         return maxSum[0];
     }
 
-    private int maxPathSumHelper(Node<E> node, int[] maxSum) {
+    private int calculateMaxPathSum(Node<E> node, int[] maxSum) {
         if (node == null) {
             return 0;
         }
 
-        int leftSum = Math.max(0, maxPathSumHelper(node.leftChild, maxSum));
-        int rightSum = Math.max(0, maxPathSumHelper(node.rightChild, maxSum));
+        int leftSum = Math.max(0, calculateMaxPathSum(node.leftChild, maxSum));
+        int rightSum = Math.max(0, calculateMaxPathSum(node.rightChild, maxSum));
 
         int currentSum = leftSum + rightSum + ((Integer) node.value);
         maxSum[0] = Math.max(maxSum[0], currentSum);
@@ -118,6 +123,7 @@ public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinary
     public String toVisualizedString() {
         StringBuilder sb = new StringBuilder();
         visualizeTree(root, 0, sb);
+
         return sb.toString();
     }
 
@@ -134,4 +140,3 @@ public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinary
         visualizeTree(node.leftChild, depth + 1, sb);
     }
 }
-
